1fc9cdc9a1741f83aa78e9bba546692496498a7e
Peter Fetros <47762798+goldenbug@users.noreply.github.com>
2019-10-12 00:39:45 -0700

    Fixed Bug with Kmeans and KDTree search.
    There was a bug with the KDTree where the data was not in the correct
    format and was therefore not being correctly parsed.
    
    Signed-off-by: Peter Fetros <47762798+GoldenBug@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -6,22 +6,23 @@
 using namespace std;
 using namespace cv;
 
-#define KMEANATTEMPTS 5
 #define SUBSECTIONSIZE 100
+//#define DEBUG
 
-inline Vec3b
-getLargestKMean(Mat &section, int numClusters) {
+Vec3b getLargestKMean(Mat section) {
 
-    Mat samples = section.reshape(0, 1); //section.rows * section.cols, 3, CV_32F);
-    samples.convertTo(samples, CV_32F);
+    Mat samples;
+    section.convertTo(samples, CV_32F);
+    samples = samples.reshape(1, samples.total());
 
     Mat center;
     Mat labels;
+    int attempts = 5;
+
+    kmeans(samples, 1, labels, TermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS,
+                                            10000, 0.0001), attempts, KMEANS_PP_CENTERS, center);
+
     Vec3b color;
-
-    kmeans(samples, numClusters, labels, TermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS,
-                                                      10000, 0.0001), KMEANATTEMPTS, KMEANS_PP_CENTERS, center);
-
     int cluster_idx = labels.at<int>(0, 0);
     color[0] = center.at<float>(cluster_idx, 0);
     color[1] = center.at<float>(cluster_idx, 1);
@@ -31,10 +32,11 @@ getLargestKMean(Mat &section, int numClusters) {
 }
 
 void
-analyzeMovieFrames(VideoCapture cap, uint32_t frameInterval, Mat &frameData, unordered_map<uint32_t, Mat> &frameCache) {
+analyzeMovieFrames(VideoCapture &cap, uint32_t frameInterval, Mat &frameData,
+                   unordered_map<uint32_t, Mat> &frameCache) {
 
     int totalNumberFrames = cap.get(CV_CAP_PROP_FRAME_COUNT);
-    frameData = Mat(totalNumberFrames / frameInterval, 1, CV_32F);
+    frameData = Mat(totalNumberFrames / frameInterval, 3, CV_32F);
 
     for (int frameNum = 0; frameNum < totalNumberFrames; frameNum += frameInterval) {
         setBarPositionBottom(false);
@@ -48,9 +50,7 @@ analyzeMovieFrames(VideoCapture cap, uint32_t frameInterval, Mat &frameData, uno
         cap >> frame;
         resize(frame, smaller, Size(SUBSECTIONSIZE, SUBSECTIONSIZE));
 
-        Vec3b color;
-        color = getLargestKMean(smaller, 1);
-
+        Vec3b color = getLargestKMean(smaller);
         frameData.at<float>(frameNum, 0) = color[0];
         frameData.at<float>(frameNum, 1) = color[1];
         frameData.at<float>(frameNum, 2) = color[2];
@@ -61,7 +61,8 @@ analyzeMovieFrames(VideoCapture cap, uint32_t frameInterval, Mat &frameData, uno
     return;
 }
 
-int main() {
+int
+main(int argc, char *argv[]) {
 
     // Load the video
     VideoCapture cap("./test_files/movies/test_video.mp4");
@@ -85,16 +86,22 @@ int main() {
     resize(poster, poster, Size(width, height));
 
     flann::KDTreeIndexParams indexParams;
-    flann::Index kdtree(frameData, indexParams);
+    flann::Index kdtree(Mat(frameData).reshape(1), indexParams);
+
+#ifdef DEBUG
+    namedWindow( "Frame", WINDOW_AUTOSIZE );
+    namedWindow( "Mean_Color", WINDOW_AUTOSIZE );
+    namedWindow( "b", WINDOW_AUTOSIZE );
+    namedWindow( "c", WINDOW_AUTOSIZE );
+#endif
 
     for (int y = 0; y < height; y += SUBSECTIONSIZE) {
         for (int x = 0; x < width; x += SUBSECTIONSIZE) {
-
             setBarPositionBottom(false);
             writeStatusBar((double) y, (double) height, "Replacing movie frames");
 
             Mat subsection = poster(Range(y, y + SUBSECTIONSIZE - 1), Range(x, x + SUBSECTIONSIZE - 1)).clone();
-            Vec3b color = getLargestKMean(subsection, 1);
+            Vec3b color = getLargestKMean(subsection);
 
             vector<int> indices;
             vector<float> dists;
@@ -109,6 +116,18 @@ int main() {
 
             closestFrame.copyTo(poster(Rect(x, y, closestFrame.cols, closestFrame.rows)));
 
+#ifdef DEBUG
+            Mat temp_frame = subsection.clone();
+            temp_frame.setTo(Scalar(color[0], color[1], color[2]));
+            color = frameData.at<Vec3b>(indices[0]);
+            Mat temp2_frame = subsection.clone();
+            temp2_frame.setTo(Scalar(color[0], color[1], color[2]));
+            imshow( "Frame", subsection);
+            imshow( "Mean_Color", temp_frame);
+            imshow( "b", closestFrame);
+            imshow( "c", temp2_frame);
+            waitKey(100);
+#endif
         }
     }
 
