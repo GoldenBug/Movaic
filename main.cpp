9e3b73ad223e2eeb9661e12615d6241aa11c3bc2
Peter Fetros <47762798+goldenbug@users.noreply.github.com>
2019-10-11 19:47:28 -0700

    Extracted Movie Analysis to a new function
    Extracted the code that performs the movie analysis to a new function.
    This function also fills the index matrix as well as creates a frame cache to
    quickly look up frames for replacing sections of the movie poster.
    
    Signed-off-by: Peter Fetros <47762798+GoldenBug@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,37 +1,67 @@
 #include "opencv2/opencv.hpp"
 #include <random>
 #include "include/HealthBarAPI.h"
+#include <stdint.h>
 
 using namespace std;
 using namespace cv;
 
-#define FRAMEINTERVAL 4
 #define KMEANATTEMPTS 5
+#define SUBSECTIONSIZE 100
 
 inline Vec3b
-getLargestKMean(const Mat &section, int numClusters) {
+getLargestKMean(Mat &section, int numClusters) {
 
-    Vec3b color;
     Mat samples = section.reshape(0, 1); //section.rows * section.cols, 3, CV_32F);
+    samples.convertTo(samples, CV_32F);
 
     Mat center;
     Mat labels;
+    Vec3b color;
 
-    unsigned int flags = (unsigned) CV_TERMCRIT_ITER | (unsigned) CV_TERMCRIT_EPS;
-    kmeans(samples, numClusters, labels, TermCriteria(flags, 10000, 0.0001), KMEANATTEMPTS, KMEANS_PP_CENTERS, center);
+    kmeans(samples, numClusters, labels, TermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS,
+                                                      10000, 0.0001), KMEANATTEMPTS, KMEANS_PP_CENTERS, center);
 
     int cluster_idx = labels.at<int>(0, 0);
-    color = center.at<Vec3b>(cluster_idx);
+    color[0] = center.at<float>(cluster_idx, 0);
+    color[1] = center.at<float>(cluster_idx, 1);
+    color[2] = center.at<float>(cluster_idx, 2);
 
     return color;
 }
 
-int main() {
+void
+analyzeMovieFrames(VideoCapture cap, uint32_t frameInterval, Mat &frameData, unordered_map<uint32_t, Mat> &frameCache) {
 
-    // Init random number generator.
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_int_distribution<int> dist(0, 10);
+    int totalNumberFrames = cap.get(CV_CAP_PROP_FRAME_COUNT);
+    frameData = Mat(totalNumberFrames / frameInterval, 1, CV_32F);
+
+    for (int frameNum = 0; frameNum < totalNumberFrames; frameNum += frameInterval) {
+        setBarPositionBottom(false);
+        writeStatusBar((double) frameNum, (double) totalNumberFrames, "Analyzing movie frames");
+
+        cap.set(CAP_PROP_POS_FRAMES, frameNum * frameInterval);
+
+        Mat frame;
+        Mat smaller;
+
+        cap >> frame;
+        resize(frame, smaller, Size(SUBSECTIONSIZE, SUBSECTIONSIZE));
+
+        Vec3b color;
+        color = getLargestKMean(smaller, 1);
+
+        frameData.at<float>(frameNum, 0) = color[0];
+        frameData.at<float>(frameNum, 1) = color[1];
+        frameData.at<float>(frameNum, 2) = color[2];
+
+        frameCache[frameNum / frameInterval] = smaller;
+    }
+
+    return;
+}
+
+int main() {
 
     // Load the video
     VideoCapture cap("./test_files/movies/test_video.mp4");
@@ -42,30 +72,11 @@ int main() {
         return -1;
     }
 
-    Mat frameData(1770, 3, CV_32F);
+    Mat frameData;
+    uint32_t frameInterval = 1;
+    unordered_map<uint32_t, Mat> frameCache;
 
-    std::string currentOp = "Analyzing movie frames";
-    for (int frameNum = 0; frameNum < 2000; ++frameNum) {
-        setBarPositionBottom(false);
-        writeStatusBar((double) frameNum, (double) 2000, currentOp);
-        Mat frame;
-        cap.set(CAP_PROP_POS_FRAMES, frameNum * FRAMEINTERVAL);
-        cap >> frame;
-
-        // If the frame is empty, break immediately
-        if (frame.empty())
-            break;
-
-        Mat smaller;
-        resize(frame, smaller, Size(400, 400));
-
-        Vec3b color;
-
-        color = getLargestKMean(smaller, 3);
-
-        frameData.at<Vec3b>(frameNum, 0) = color;
-
-    }
+    analyzeMovieFrames(cap, frameInterval, frameData, frameCache);
 
     const int width = 12000;
     const int height = 18000;
@@ -73,52 +84,35 @@ int main() {
     Mat poster = imread("./test_files/posters/test_poster.jpg", CV_LOAD_IMAGE_COLOR);
     resize(poster, poster, Size(width, height));
 
-    int subSectionSize = 100;
-    unordered_map<uint32_t, vector<int>> frameMap;
-    unordered_map<uint32_t, Mat> frameCache;
-    for (int y = 0; y < height; y += subSectionSize) {
-        for (int x = 0; x < width; x += subSectionSize) {
+    flann::KDTreeIndexParams indexParams;
+    flann::Index kdtree(frameData, indexParams);
 
-            Mat subsection = poster(Range(y, y + subSectionSize - 1), Range(x, x + subSectionSize - 1));
+    for (int y = 0; y < height; y += SUBSECTIONSIZE) {
+        for (int x = 0; x < width; x += SUBSECTIONSIZE) {
+
+            setBarPositionBottom(false);
+            writeStatusBar((double) y, (double) height, "Replacing movie frames");
+
+            Mat subsection = poster(Range(y, y + SUBSECTIONSIZE - 1), Range(x, x + SUBSECTIONSIZE - 1)).clone();
             Vec3b color = getLargestKMean(subsection, 1);
 
             vector<int> indices;
+            vector<float> dists;
+            vector<float> query;
+            query.push_back(color[0]);
+            query.push_back(color[1]);
+            query.push_back(color[2]);
 
-            uint32_t key = ((unsigned) color[0] << 16) + ((unsigned) color[1] << 8) + (unsigned) color[2];
+            kdtree.knnSearch(query, indices, dists, 1);
 
-            if (frameMap.find(key) != frameMap.end()) {
-                indices = frameMap[key];
-            } else {
+            Mat closestFrame = frameCache[indices[0]];
 
-                flann::KDTreeIndexParams indexParams;
-                flann::Index kdtree(Mat(frameData).reshape(1), indexParams);
-                vector<float> query;
-                query.push_back(color[0]);
-                query.push_back(color[1]);
-                query.push_back(color[2]);
-                vector<float> dists;
-                kdtree.knnSearch(query, indices, dists, 10);
-                frameMap[key] = indices;
-            }
-
-            Mat closestFrame;
-            int chosenFrame = indices[dist(mt)] * FRAMEINTERVAL;
-
-            if (frameCache.find(chosenFrame) != frameCache.end())
-                closestFrame = frameCache[chosenFrame];
-            else {
-                cap.set(CAP_PROP_POS_FRAMES, indices[dist(mt)] * FRAMEINTERVAL);
-                cap >> closestFrame;
-                frameCache[chosenFrame] = closestFrame;
-            }
-
-            resize(closestFrame, closestFrame, Size(subSectionSize, subSectionSize));
             closestFrame.copyTo(poster(Rect(x, y, closestFrame.cols, closestFrame.rows)));
 
         }
     }
 
-    imwrite("../movie/gatsbyPoster2_movaic.jpg", poster);
+    imwrite("./test_files/movies/output.jpg", poster);
     cout << "done" << endl;
 
     // When everything done, release the video capture object
